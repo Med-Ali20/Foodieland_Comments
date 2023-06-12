0b4fa86845e5fc8975ea60d4695b258ea5de0a93
Ahmed Ali <alii403304@gmail.com>
2023-06-12 18:55:24 +0200

    initial version

---
+++ controller.js
@@ -0,0 +1,111 @@
+const axios = require("axios");
+
+// Airtable Data -- FB
+const airtableAccessToken =
+  process.env.AIRTABLE_ACCESS_TOKEN;
+const baseId = process.env.BASE_ID;
+const tableId = process.env.TABLE_ID;
+
+//--IG
+
+const instagramTableId = process.env.INSTAGRAM_TABLE_ID;
+
+// Airtable Functions
+
+const createAirtableRecord = async (fields, platform) => {
+  try {
+    const res = await axios.post(
+      `https://api.airtable.com/v0/${baseId}/${ platform === 'FB' ? tableId : instagramTableId} `,
+      {
+        fields,
+        typecast: true,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${airtableAccessToken}`,
+        },
+      }
+    );
+    console.log("record created");
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAirtableRecords = async (offset, platform) => {
+  try {
+    const res = await axios.get(
+      `https://api.airtable.com/v0/${baseId}/${platform === "FB" ? tableId : instagramTableId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${airtableAccessToken}`,
+        },
+        params: {
+          offset: offset ? offset : null,
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function getAirtableComments(platform) {
+  let offset;
+  let comments = []; // An array to hold all the data
+
+  while (true) {
+    // Keep looping until all the data is fetched
+    await new Promise((resolve) => setTimeout(resolve, 200)); // Add a delay of 300ms
+
+    const response = await getAirtableRecords(offset, platform);
+    const records = response.records;
+    comments = comments.concat(records); // Add the data to the array
+
+    if (!response.offset) {
+      // No more data to fetch, exit loop
+      break;
+    }
+
+    offset = response.offset; // Move to the next page
+  }
+  return comments; // Return all the fetched data
+}
+
+const createComment = async (comment) => {
+  try {
+    const airtableComments = await getAirtableComments("FB");
+    const commentId = comment["Comment ID"];
+    const existingComments = airtableComments.filter(
+      (comment) => commentId === comment.fields["Comment ID"]
+    );
+    if (existingComments.length == 0) {
+      createAirtableRecord(comment, "FB");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const createInstagramComment = async(comment) => {
+  try {
+    const airtableComments = await getAirtableComments("IG");
+    const commentId = comment["Comment ID"];
+    const existingComments = airtableComments.filter(
+      (comment) => commentId === comment.fields["Comment ID"]
+    );
+    if (existingComments.length == 0) {
+      createAirtableRecord(comment, "IG");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  createComment,
+  createInstagramComment
+};
---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+require("dotenv").config();
+const { createComment, createInstagramComment } = require("./controller");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/meta_comments", async (req, res) => {
+  try {
+    const challenge = req.query["hub.challenge"];
+    res.status(200).send(challenge);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/meta_comments", async (req, res) => {
+  try {
+    console.log(req.body.entry[0].changes[0].value.message);
+    if (req.body.entry[0].changes[0].value.comment_id) {
+      if (req.body.entry[0].changes[0].value.message.length == 0) {
+        return;
+      }
+      const date = new Date(
+        req.body.entry[0].changes[0].value.created_time * 1000
+      );
+      const createdAt = date.toISOString();
+
+      const comment = {
+        Comment: req.body.entry[0].changes[0].value.message,
+        "Post ID": req.body.entry[0].changes[0].value.post_id,
+        "Comment ID": req.body.entry[0].changes[0].value.comment_id,
+        "Poster ID": req.body.entry[0].changes[0].value.from.id,
+        "Poster Name": req.body.entry[0].changes[0].value.from.name,
+        "Created At": createdAt,
+      };
+
+      createComment(comment);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Instagram Comments
+
+app.get("/instagram_comments", async (req, res) => {
+  try {
+    const challenge = req.query["hub.challenge"];
+    res.status(200).send(challenge);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/instagram_comments", async (req, res) => {
+  try {
+    if (
+      req.body.object === "instagram" &&
+      req.body.entry[0].changes[0].field === "comments"
+    ) {
+      if(req.body.entry[0].changes[0].value.text.length == 0) {return}
+      console.log(req.body);
+      const date = new Date(req.body.entry[0].time * 1000);
+      const createdAt = date.toISOString();
+      const comment = {
+        Comment: req.body.entry[0].changes[0].value.text,
+        "Comment ID": req.body.entry[0].changes[0].value.id,
+        "Media ID": req.body.entry[0].changes[0].value.media.id,
+        "Poster ID": req.body.entry[0].changes[0].value.from.id,
+        "Poster Name": req.body.entry[0].changes[0].value.from.username,
+        "Created At": createdAt,
+      };
+      createInstagramComment(comment);
+      console.log(comment);
+    }
+  } catch (error) {
+    console.log("instagram webhook error", error);
+  }
+});
+
+app.listen(PORT, async () => {
+  console.log(`App is running on port ${PORT}`);
+  console.log(process.env.airtableAccessToken)
+});
